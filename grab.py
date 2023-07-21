52580059cb51384f9f2703f444cd1269dc34ce7c
zeron-G <2950243695@qq.com>
2023-07-22 00:08:15 +0800

    2

--- grab.py
+++ grab.py
@@ -3,6 +3,8 @@ import yfinance as yf
 start_date = "2003-01-01"
 end_date = "2023-01-01"
 NASDAQ=yf.download('^IXIC', start=start_date, end=end_date)
-SS=yf.download('000001.SS', start=start_date, end=end_date)
+DJI=yf.download('DJI', start=start_date, end=end_date)
 NASDAQ.to_csv('data/NASDAQ.csv')
-SS.to_csv('data/000001ss.csv')
+DJI.to_csv('data/DJIA.csv')
+SPX=yf.download('^GSPC', start=start_date, end=end_date)
+SPX.to_csv('data/SPX.csv')
