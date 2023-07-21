d91051a60b79a19e80bdbd6d30552be15732cbf4
zeron-G <2950243695@qq.com>
2023-07-21 22:32:39 +0800

    3

--- grab.py
+++ grab.py
@@ -1,12 +1,8 @@
 #following code use python to grab the stock index data from Yahoo Finance
 import yfinance as yf 
-start_date = "1995-02-20"
-end_date = "2023-07-17"
-sz = yf.download('399001.SZ', start=start_date, end=end_date)
-DJIA=yf.download('DJI', start=start_date, end=end_date)
+start_date = "2003-01-01"
+end_date = "2023-01-01"
 NASDAQ=yf.download('^IXIC', start=start_date, end=end_date)
 SS=yf.download('000001.SS', start=start_date, end=end_date)
-sz.to_csv('data/399001sz.csv')
-DJIA.to_csv('data/DJIA.csv')
 NASDAQ.to_csv('data/NASDAQ.csv')
 SS.to_csv('data/000001ss.csv')
